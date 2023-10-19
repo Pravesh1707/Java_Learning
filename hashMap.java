6252c783e1b8d3018fd56f5ae0424ffccc42f8c1
Pravesh Kumar <pkr17072000@gmail.com>
2023-10-19 15:41:01 +0530

    Carelon Test 2024

--- hashMap.java
+++ hashMap.java
@@ -1,31 +1,38 @@
 import java.util.*;
 
+// Carelon 2024 Test
 public class hashMap {
 	public static void main(String[] args) {
 
-	int arr[] = {2,4,2,3,3};
-	int max = 0;
-	int maxChar = 0;
+	String  arr[] = {"hello","Good"};
+	int len = arr.length;
+	// char maxChar = 0;
 
-	HashMap<Integer, Integer> hm = new HashMap<>();
-
-		for (int a : arr) {
-//			char a = arr[i];
+	
+	int i=0;
+	while(len-->0){
+		HashMap<Character, Integer> hm = new HashMap<>();
+		
+			String str =  arr[i];
+			for(char a :str.toCharArray())
 			if (hm.containsKey(a)) {
 				int val = hm.get(a);
 				hm.put(a, val + 1);
 			} else {
 				hm.put(a, 1);
 			}
-		}
 
-		for (int a: hm.keySet()) {
+		char maxChar = ' ';
+		int max = Integer.MIN_VALUE;
+		for (char a: hm.keySet()) {
 			if (hm.get(a) > max) {
 				max = hm.get(a);
 				maxChar = a;
 			}
 		}
-		System.out.println("max is :  " + max);
-		System.out.println("max char is :  " + maxChar);
+		i++;
+		System.out.println(maxChar);
+	}
+	// System.out.println();
 	}
 }
